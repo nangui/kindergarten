92491a52a8a84fdb019a870b24fdfd5222d151ab
Adonai Elohim <adonainangui03@gmail.com>
2021-09-18 01:16:24 +0000

    Create controllers

---
+++ app/Http/Controllers/PupilController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pupil;
+use Illuminate\Http\Request;
+
+class PupilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pupil  $pupil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pupil $pupil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pupil  $pupil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pupil $pupil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pupil  $pupil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pupil $pupil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pupil  $pupil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pupil $pupil)
+    {
+        //
+    }
+}
--- app/Models/Regulation.php
+++ app/Models/Regulation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Regulations extends Model
+class Regulation extends Model
 {
     use HasFactory;
 }
