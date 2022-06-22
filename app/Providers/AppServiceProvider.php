950f502a1b15caa9a7ac21e0f6c2f6cac06a4285
Adonai Elohim <adonainangui03@gmail.com>
2022-06-22 11:44:06 +0000

    Add ability to create default user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\View\Components\Invoice\InvoiceContent;
 use App\View\Components\Invoice\ReceiptContent;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,6 +31,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (User::count() == 0) {
+            User::create([
+                'name' => 'Administrateur',
+                'email' => 'admin@ponda.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('passer@123'),
+                'remember_token' => 'azertyuiop'
+            ]);
+        }
         Carbon::setLocale(config('app.locale')); // fr_FR
         Blade::component('invoice-content', InvoiceContent::class);
         Blade::component('receipt-content', ReceiptContent::class);
